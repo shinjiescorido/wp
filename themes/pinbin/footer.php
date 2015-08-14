64b2e8c86c896fa593103fed5b5c117ce3f8556e
shinjiescorido <shinji.escorido@gmail.com>
2015-08-13 21:41:21 -0400

    initial commit

---
+++ themes/pinbin/footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ */
+
+?>
+
+<?php if ( is_active_sidebar( 'pinbin_footer')) { ?>     
+   <div id="footer-area">
+			<?php dynamic_sidebar( 'pinbin_footer' ); ?>
+        </div><!-- // footer area with widgets -->   
+<?php }  ?>           
+<footer class="site-footer">
+	 <div id="copyright">
+		<?php _e( 'Copyright', 'pinbin' ) ?> <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> |
+		<?php _e( 'Powered by' , 'pinbin' ); ?> <a href="http://www.wordpress.org" target="_blank" title="<?php _e( 'Powered by WordPress' , 'pinbin' ); ?>">WordPress</a>
+	 </div><!-- // copyright -->   
+</footer>     
+</div><!-- // close wrap div -->   
+
+<?php wp_footer(); ?>
+	
+</body>
+</html>
+
